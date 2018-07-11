9c3d9f9a58498dc0572204058b65752b9aeca3bd
dalijian <237922011@qq.com>
2018-07-11 21:45:46 +0800

    this in the master

--- hello.java
+++ hello.java
@@ -2,5 +2,8 @@ public class hello{
    public static void main(String[] args) {
       System.out.println("hello world"); 
       System.out.println("git branch dev"); 
+
+      
+      System.out.println("this in the master");
     }
 }
